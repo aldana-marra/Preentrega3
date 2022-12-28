6e9955e45d61777f29340570565b766334bd9413
Aldana Marra <aldana.marra@hotmail.com>
2022-12-28 18:07:28 -0300

    preentrega3

---
+++ src/components/data/stock.js
@@ -0,0 +1,65 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "churros",
+        tipo: "churros",
+        desc: "Deliciosos churros con dulce de leche",
+        precio: 700,
+        img: '/public/images/churros.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Cupcackes",
+        tipo: "cupcacke",
+        desc: "Los cupcakes no son otra cosa que pastelitos individuales",
+        precio: 500,
+        img: '/public/images/cupcackes.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Macaroons",
+        tipo: "macaroons",
+        desc: "Dulces densos hechos con almendra y clara de huevo",
+        precio: 1000,
+        talle: "S",
+        img: '/public/images/macaroons.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Medialunas",
+        tipo: "medialunas",
+        desc: "Autenticas y clasicas medialunas con un toque gourmet",
+        precio: 800,
+        talle: "L",
+        img: '/public/images/medialunas.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Pan Dulce",
+        tipo: "pan dulce",
+        desc: "Un clasico navideño que se disfruta todo el año",
+        precio: 3000,
+        img: '/public/images/pandulce.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Torta",
+        tipo: "torta",
+        desc: "Torta de arándanos frescos con un twist",
+        precio: 1500,
+
+        img: '/public/images/torta.jpg',
+        cantidad:1
+    }
+];
